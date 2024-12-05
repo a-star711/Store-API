4789a3bb8a6cf188ff23a86fc1442c48c567135c
a-star711 <a.krasimirov35@gmail.com>
2024-12-05 11:03:47 +0200

    Implement/test more complex queries

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,32 @@
-const getAllProductsStatic = async (req,res) => {
-  res.status(200).json({msg:'products testing route'})
+const Product = require('../models/product')
 
+const getAllProductsStatic = async (req,res) => {
+  const search = 'ab'
+
+  const products = await Product.find({
+    // i stands for case insesitive
+    name: { $regex: search, $options: 'i' }
+  })
+  res.status(200).json({ products, nbHits: products.length })
 }
 
-const getAllProducts = async (req,res) => {
-  res.status(200).json({msg:'products route'})
+  const getAllProducts = async (req,res) => {
 
+  const {featured, company, name} = req.query   
+  const queryObject = {}
+
+  if(featured){
+    queryObject.featured = featured === "true" ? true : false 
+  }
+  if(company){
+    queryObject.company = company;
+  }
+  if(name) {
+    queryObject.name = { $regex: name, $options: 'i' }
+  }
+  console.log('queryObj', queryObject)
+  const products = await Product.find(queryObject)
+  res.status(200).json({ products, nbHits: products.length })
 }
 
 module.exports = {
