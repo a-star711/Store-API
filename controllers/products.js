b407618e84259b31aa052cf003b724f42d6d3b35
a-star711 <a.krasimirov35@gmail.com>
2024-12-05 15:01:51 +0200

    Filter specific fields query & Pagination

--- controllers/products.js
+++ controllers/products.js
@@ -3,14 +3,14 @@ const Product = require('../models/product')
 const getAllProductsStatic = async (req,res) => {
 
 
-  const products = await Product.find({}).sort('-name price')
+  const products = await Product.find({price:{ $gt:30 }}).sort('price').select('name price').limit(4)
 
   res.status(200).json({ products, nbHits: products.length })
 }
 
   const getAllProducts = async (req,res) => {
 
-  const {featured, company, name, sort} = req.query   
+  const {featured, company, name, sort, fields} = req.query   
   const queryObject = {}
 
   if(featured){
@@ -21,16 +21,33 @@ const getAllProductsStatic = async (req,res) => {
   }
   if(name) {
     queryObject.name = { $regex: name, $options: 'i' }
-  }
-  // console.log('queryObj', queryObject)
+  } 
+   // sorting
   let result = Product.find(queryObject)
   if(sort){
-    console.log(sort)
+   
     const sortList = sort.split(',').join(' ')
     result = result.sort(sortList)
   } else {
     result = result.sort('createdAt')
   }
+  // selecting specific fields from the model to return from query
+  if(fields) {
+    const fieldsList = fields.split(',').join(' ')
+    result = result.select(fieldsList)
+  }
+
+  // pagination
+
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  // 23 products atm / 7 = 4 pages; 7 7 7 2 products per page
+
+
   const products = await result
   res.status(200).json({ products, nbHits: products.length })
 }
