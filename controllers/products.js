0eb3da03c54807a79b4ab2376d9b3c19238d7550
a-star711 <a.krasimirov35@gmail.com>
2024-12-05 11:17:43 +0200

    Implement sorting

--- controllers/products.js
+++ controllers/products.js
@@ -1,18 +1,16 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req,res) => {
-  const search = 'ab'
 
-  const products = await Product.find({
-    // i stands for case insesitive
-    name: { $regex: search, $options: 'i' }
-  })
+
+  const products = await Product.find({}).sort('-name price')
+
   res.status(200).json({ products, nbHits: products.length })
 }
 
   const getAllProducts = async (req,res) => {
 
-  const {featured, company, name} = req.query   
+  const {featured, company, name, sort} = req.query   
   const queryObject = {}
 
   if(featured){
@@ -24,8 +22,16 @@ const getAllProductsStatic = async (req,res) => {
   if(name) {
     queryObject.name = { $regex: name, $options: 'i' }
   }
-  console.log('queryObj', queryObject)
-  const products = await Product.find(queryObject)
+  // console.log('queryObj', queryObject)
+  let result = Product.find(queryObject)
+  if(sort){
+    console.log(sort)
+    const sortList = sort.split(',').join(' ')
+    result = result.sort(sortList)
+  } else {
+    result = result.sort('createdAt')
+  }
+  const products = await result
   res.status(200).json({ products, nbHits: products.length })
 }
 
